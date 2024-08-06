4e2e0eb51f6d9cab691dca6c147b1003a0965950
Trullexboy <117014512+TrullexBoy@users.noreply.github.com>
2024-08-06 19:06:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
     TG_BOT_TOKEN = environ.get("TG_BOT_TOKEN", "")
     APP_ID = int(environ.get("APP_ID", "21288218"))
     API_HASH = environ.get("API_HASH", "dd47d5c4fbc31534aa764ef9918b3acd")
-    TG_USER_SESSION = "BQEMD9UAqDNSm-5mLHtmzttMg6-QiNr34DFHs-eHy_Ldld-PkrtaQtM0DHp8gTKYIUHy2fYrrp1AzzFdQ1PcH8bdxo7-nVRPJOhDmQWSoA0G0tHAOOKHB_5ktgg3R2yfE5XqKy0f24e7hyP-WsJxGJBz4JiYIIVgZesLrcl9eqy5fpRINpO2chqoMePZx7Er2txJbDO0JWnNOJKevY69Z9cNofQBrF_tEUnUbwAh36LhEn5ghUntjmi_48xLD6ODvTSj3TQMClYpeTv4cgN2plAGTb9bfnJT99k-9sQ8lfZypvmPW5AeiGZSURa5uKJ0ZwFUNfJGTE2AuyddX9Q6eXGcCpbu_wAAAAE9gIqFAA"
+    TG_USER_SESSION = "BQEMD9UACuvyCI_Z0enjdgiBmYaKPCsqog5qMPkNAFYSbLJjh1LZWJykWCnxOWpDyI43b7KSaTLi6opKqUBRdxuDzJRE7OY6PXcgpR87PP-wva-mKjZ-CpXGqLJ1AqUgVUqjBdX8FA4l_2ixnubrWEYTz_HgJis4jNUrIOCjs6XqwxGwjF_GCIi7vRGBJhug4VDOYjJ1v1l0rloeKwCgMgNmSImKFBoXmqJRkLAWsV7RgIliNU-UCNQBSdmGDu-SIIxHCMQfeIjw1n6v8nrRkIgUJeTUI5fdafJShCP21jDxMoSggV_jD9-Z_-SPvVlRG4KsZHPi5Au62bz6OSSDu94cTJOU_wAAAAE9gIqFAA"
     FILE_CAPTION = environ.get('FILE_CAPTION', '<code>{file_name}</code>')
     ADMINS = [x.strip("@ ") for x in str(environ.get("ADMINS", "") or "").split(",") if x.strip("@ ")]
     
