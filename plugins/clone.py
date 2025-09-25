7aa78d6be1c11d34cc469b9bdeb576f1c0658b23
Mx 0980 <133099256+mx0980@users.noreply.github.com>
2025-09-26 02:50:50 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -79,7 +79,7 @@ async def forward_files(lst_msg_id, chat, msg, bot, user_id):
     deleted = 0
     unsupported = 0
     fetched = 0
-    to_channel = -1001976541395
+    to_channel = -1001447817749
     try:
         skip_idd = await bot.get_messages(-1001631481154, 9)
     except Exception as e:
