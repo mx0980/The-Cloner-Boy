dca249e253dafc324f3c39e68732cdc2b06b42ab
Mx 0980 <133099256+mx0980@users.noreply.github.com>
2026-05-01 00:14:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
     TG_BOT_TOKEN = environ.get("TG_BOT_TOKEN", "")
     APP_ID = int(environ.get("APP_ID", "21288218"))
     API_HASH = environ.get("API_HASH", "dd47d5c4fbc31534aa764ef9918b3acd")
-    TG_USER_SESSION = "BQEMD9UAR98zcKfgfvVihxeL_g41R7dVmRuOhu095dUYGsrnsH5p-GNNsV8PMqrYU9PlIlUujIGAS31sSkBVEvFvh4Eh6H5MloziffkE9kk9mt6Gu0Vt6i13JUFhfThlYmJPgzfHrNqWofrVghwMM31aKb0N_QAm7kQ97BaSs3LRBCj-nzNUXBV2k7qfsvPhGAeEL27fkYeALNa3vpEG0816AALDAIeVdN1OUH1O-9q93Y7qTOoJARvy89sQqqOn5Al8wEi_HkDuWGxWbxakyO690hEfRml7KI90tHY-HcvdIWcl1MCnC7QUXltnlNxZ370jXlq3pkDrsyTKfJKWK02bD0CHowAAAAE9gIqFAA"
+    TG_USER_SESSION = environ.get("TG_USER_SESSION", "")
     FILE_CAPTION = environ.get('FILE_CAPTION', '<code>{file_name}</code>')
     ADMINS = [x.strip("@ ") for x in str(environ.get("ADMINS", "") or "").split(",") if x.strip("@ ")]
     STATUS_CHAT_GROUP_ID = -1001212782000
